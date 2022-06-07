53b17ad03fedef908c0cb4d70e0cc0b467c38ca7
FlokkoVoid <fjdemcer@gmail.com>
2022-06-07 13:44:21 +0200

    Update

--- Terraininator/Assets/Brush/mouseInput.cs
+++ Terraininator/Assets/Brush/mouseInput.cs
@@ -8,10 +8,12 @@ public class mouseInput : MonoBehaviour
     ChunkSystem chunkSystem;
 
     Brush brush;
-    int radius;
-    int power;
-    Color colour;
+    public int radius;
+    public int power;
+    public Color colour;
     public bool isCircle;
+    private float timer;
+    private float wait;
 
     void Start() {
         brush = new BrushFlatten();
@@ -19,20 +21,27 @@ public class mouseInput : MonoBehaviour
         power = -5;
         isCircle = true;
         colour = Color.magenta;
+        timer = 0f;
+        wait = 1f/3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Left mouse button is clicked.
-        if(Input.GetMouseButtonDown(0)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitData;
-            if (Physics.Raycast(ray, out hitData)){
-                Vector3 hitPosition = hitData.point;
+        timer += Time.deltaTime;
+        if(timer >= wait) {
+            timer = 0;
 
-                //Do something
-                brush.Apply(chunkSystem, hitData, this.radius, this.power, this.colour, this.isCircle);
+            //Left mouse button is clicked.
+            if(Input.GetMouseButtonDown(0)){
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitData;
+                if (Physics.Raycast(ray, out hitData)){
+                    Vector3 hitPosition = hitData.point;
+
+                    //Do something
+                    brush.Apply(chunkSystem, hitData, this.radius, this.power, this.colour, this.isCircle);
+                }
             }
         }
     }
